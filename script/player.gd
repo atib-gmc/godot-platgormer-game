c4808a09e7e819aff0990eae6f7c6e9f7e5e75ac
atib <atibLpu@gmail.com>
2024-06-04 18:01:45 +0700

    chore:add paralax and killzone

--- script/player.gd
+++ script/player.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+
+const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player = "$AnimatableBody2D"
+
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -12,11 +17,13 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	print(direction)
+
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
